658650c88730a8b976fac9c49225fee4e9017bcd
Maique Douglas Moraes da Silva <maique.silva@digitro.com>
2021-11-24 15:07:03 -0300

    first

---
+++ src/main/java/com/content/StepDefinitions/Steps.java
@@ -0,0 +1,29 @@
+package com.content.StepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Steps {
+
+    @Given("the user is on login page")
+    public void the_user_is_on_login_page() {
+        System.out.printf("The user is on login page");
+    }
+
+    @When("the user enters valid credentials")
+    public void the_user_enters_valid_credentials() {
+        System.out.printf("Entered username and password");
+    }
+
+    @When("hits submit")
+    public void hits_submit() {
+        System.out.printf("Clicked on submit");
+    }
+
+    @Then("the user should be logged in successfully")
+    public void the_user_should_be_logged_in_successfully() {
+        System.out.printf("Yeah I am logged in");
+    }
+
+}
